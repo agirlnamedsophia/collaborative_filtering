296a8bcf4645c308c00384283455f0af5b9b9517
Andrew Kelleher <keats.kelleher@gmail.com>
2016-03-04 17:04:51 -0500

    Fixes modes, installs services

--- deploy/daemontools_svc.py
+++ deploy/daemontools_svc.py
@@ -1,43 +1,54 @@
 from ansible.module_utils.basic import * 
 
 import os
-import sys
 import pwd
-import shutil
+import hashlib
 
 DOCUMENTATION = """
 ---
 module: daemontools_svc
+author: Andrew Kelleher <keats.kelleher@gmail.com>
 short_description: This module deploys services under daemontools/svc
+options:
+    state:
+        description: The desired state of the service. Options are 'started' or 'absent'
+        required: true
+    setup_cmd:
+        description: The command to configure the environment in which the daemon will run.
+        required: false
+    user:
+        description: The unprivileged user who will run the daemon
+        required: false
+    run_cmd:
+        description: The command to invoke your service. NOTE: The service should run in the foreground. Be sure to invoke the one in the local directory, not your code repository.
+        required: true
+    daemon_path:
+        description: The path to the standalone file or directory from which your service will be copied.
+        required: true
+    name:
+        description: A human readable name for your service that will be used to prefix it in the /service directory
+        required: true
+    count:
+        description: The number of instances of the service to run.
+        required: true
+"""
+
+EXAMPLES = """
+name: "Deploy some service called foo.py"
+daemontools_svc:
+    state=started
+    setup_cmd="export APP_ENVIRONMENT=dev"
+    user=cfuser
+    run_cmd="/local/venv/collaborative_filtering/bin/python /collaborative_filtering/local/foo.py"
+    daemon_path="/opt/personal/collaborative_filtering/collaborative_filtering.py"
+    name=foo
+    count=2
 """
 
 """
-For the test/debug cycle ansible provides ansible/hacking/test-module in their repository located at http://github.com/ansible/ansible
-
-You can run with the following command:
-
-```
->> ansible/hacking/test-module -m collaborative_filtering/deploy/daemontools_svc.py -a '{"state": "started", "setup_cmd": "export APP_ENVIRONMENT=stage;", "virtualenv": "/opt/local/venv/collaborative_filtering", "user": "cfuser", "args": {"lookupd-http-address": "127.0.0.1:4151"}, "daemon_path": "/opt/collaborative_filtering/collaborative_filtering.py", "name": "collaborative_filtering", "count": 5}'
-```
-
-This will generate a file at ~/.ansible_module_generated you can then invoke with python to see how it runs:
-
-```
-python ~/.ansible_module_generated    
-```
-
-You'll need to re-generate the module each time you make a change. 
-
-Documentation about this process can be found here: 
- * https://github.com/ansible/ansible/tree/devel/hacking
- * http://docs.ansible.com/ansible/developing_modules.html
- * (example) https://github.com/ansible/ansible-modules-core/blob/devel/commands/command.py
-
-You can find documentation about the actual daemontools/svc implementation here: 
-https://cr.yp.to/daemontools/faq/create.html
-
+For this project we have these defined statically. To generalize this we should allow these values
+to be passed as parameters
 """
-
 LOCAL_DIRECTORY = '/collaborative_filtering/local'
 LOCAL_SERVICE_DIRECTORY = '/collaborative_filtering/service'
 DAEMONTOOLS_SERVICE_DIRECTORY = '/service'
@@ -47,13 +58,15 @@ RUN = """
 #!/bin/sh
 exec 2>&1;
 {setup_cmd}
-{chuser} {interpreter} {daemon_path} {args}
+{chuser} {run_cmd}
 """
 
 LOG = """
 #!/bin/sh
 exec {chuser} multilog t ./main
 """
+
+
 def test_writeable(path, module): 
     try:
         with open('{}/test'.format(path), 'w+'):
@@ -63,14 +76,55 @@ def test_writeable(path, module):
         module.fail_json(
                 msg="{} should exist and be writeable by the deploy user".format(path)) 
 
+
+def export_daemon(module, daemon_path):
+    rc, stdout, stderr = module.run_command("rsync -a --copy-dirlinks --delete {} {}".format(daemon_path, LOCAL_DIRECTORY))
+    if rc:
+        module.fail_json(msg="Rsync failed to copy the daemon application.")
+    if stdout.strip():
+        return True
+    return False
+
+
+def create_directory(path):
+    if os.path.exists(path):
+        return False
+    os.makedirs(path)
+    return True
+
+
+def write_file(filepath, contents):
+    before = ""
+    try:
+        with open(filepath, 'rb') as fp:
+            before = hashlib.sha1(fp.read()).hexdigest
+    except IOError:
+        pass
+
+    after = hashlib.sha1(contents).hexdigest
+    if before != after:
+        with open(filepath, 'w+') as fp:
+            fp.write(contents)
+            os.chmod(filepath, 0755)
+        return True
+    return False
+
+
+def create_symlink(source, link_name):
+    try:
+        os.symlink(source, link_name)
+    except OSError:
+        return False
+    return True
+
+
 def main():
     module = AnsibleModule(
         argument_spec = {
-            'state': {'default': 'started', 'choices': ['started', 'stopped']},
-            'setup_cmd': {}, 
-            'virtualenv': {},
+            'state': {'default': 'started', 'choices': ['started', 'absent']},
+            'setup_cmd': {},
             'user': {},
-            'args': {'type': 'dict'},
+            'run_cmd': {'required': True},
             'daemon_path': {'required': True},
             'name': {'required': True},
             'count': {'default': 1, 'type': 'int'},
@@ -79,15 +133,14 @@ def main():
 
     state = module.params['state']
     setup_cmd = module.params['setup_cmd']
-    virtualenv = module.params['virtualenv']
     user = module.params['user']
-    args = module.params['args']
+    run_cmd = module.params['run_cmd']
     daemon_path = module.params['daemon_path']
     name = module.params['name']
     count = module.params['count']
 
-    if daemon_path.strip() == "":
-        module.fail_json(msg="The path to clone your service is required.")
+    if daemon_path.strip() == "" or not os.path.exists(daemon_path.strip()):
+        module.fail_json(msg="The path to your service is required.")
     if name.strip() == "":
         module.fail_json(msg="You must name your service.")
     if not isinstance(count, int):
@@ -96,25 +149,19 @@ def main():
         module.fail_json(msg="State should be either 'started' or 'stopped'")
 
     chuser = "setuidgid {}".format(user.strip()) if user.strip() else ""
-    interpreter = "{}/bin/python".format(virtualenv.strip()) if virtualenv.strip() else sys.executable
     daemon_path = daemon_path.strip()
-    args = " ".join(["--{}={}".format(k, v) for k, v in args.items()])
 
     run = RUN.format(setup_cmd=setup_cmd, 
-            chuser=chuser, 
-            interpreter=interpreter, 
-            daemon_path=daemon_path, 
-            args=args) 
+                     chuser=chuser,
+                     run_cmd=run_cmd)
     log = LOG.format(chuser=chuser)
 
+    changed = False
     if state == 'started':
-        # Check for writeablility of local path to copy daemon to. /collaborative_filtering/service
+        # Test whether or not the deploy will succeed before making any changes
         test_writeable(LOCAL_DIRECTORY, module)
-        # Check writeability of path to install run script (that references the daemon script in the run command)
         test_writeable(LOCAL_SERVICE_DIRECTORY, module)
-        # Check that we can link in /service
         test_writeable(DAEMONTOOLS_SERVICE_DIRECTORY, module)
-        # Check that the user exists and make the copy of our daemon executable by them
         if user.strip():
             try:
                 pwd.getpwnam(user.strip())
@@ -122,37 +169,26 @@ def main():
                 module.fail_json(msg="The user, {}, does not exist.".format(user.strip()))
 
         # Sync daemon application
-        module.run_command("rsync -a --copy-dirlinks --delete {} {}".format(daemon_path, LOCAL_DIRECTORY))
-        
-        # Write service files/dirs
-        service_directories = []
+        if export_daemon(module, daemon_path):
+            changed = True
+
+        # Install service to /service
         for i in xrange(count):
             service_directory = os.path.join(LOCAL_SERVICE_DIRECTORY, '{}-{}'.format(name, i))
-            service_directories.append(service_directory)
-            try:
-                os.makedirs(service_directory)
-            except OSError:
-                pass # Exists
-            with open(os.path.join(service_directory, 'run'), 'w+') as run_fp:
-                run_fp.write(run)
             log_directory = os.path.join(service_directory, 'log')
-            try:
-                os.makedirs(log_directory)
-            except OSError:
-                pass # Exists
-            with open(os.path.join(log_directory, 'run'), 'w+') as log_fp:
-                log_fp.write(log)
 
-        # Symlink in services
-        for service_directory in service_directories:
-            try:
-                os.symlink(service_directory, os.path.join(DAEMONTOOLS_SERVICE_DIRECTORY, os.path.basename(service_directory))) 
-            except OSError:
-                pass # Exists
+            changed = any([
+                create_directory(service_directory),
+                create_directory(log_directory),
+                write_file(os.path.join(service_directory, 'run'), run),
+                write_file(os.path.join(log_directory, 'run'), log),
+                create_symlink(service_directory,
+                               os.path.join(DAEMONTOOLS_SERVICE_DIRECTORY, os.path.basename(service_directory)))
+            ]) or changed
     else:
         return module.fail_json(msg="Removing services is not yet implemented. You have to do it manually.")
 
-    module.exit_json(changed=True)
+    module.exit_json(changed=changed)
 
 if __name__ == '__main__':
     main()
