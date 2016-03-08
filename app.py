0416a827d63cad2d7f86679d54ba8da0f657af00
Sophia Russell <sophia.elizabeth.russell@gmail.com>
2016-03-08 09:03:09 -0500

    WIP api endpoints for POST/event and POST/event_metrics (naming TBD)

--- app.py
+++ app.py
@@ -7,18 +7,46 @@ class MainHandler(tornado.web.RequestHandler):
         self.write("Hello tornado!")
 
 
-class InteractionEventHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('GET - Susie will like this because Jane did, too')
+class EventHandler(tornado.web.RequestHandler):
+    """ This will POST to the database the players
+    in an interaction that will then be analyzed.
+    """
+    def post(self, actor, entity):
+        try:
+            self.write('POST with an actor and entity')
+        except OSError as e:
+            self.respond(e.message, e.code)
+
+
+class EventMetricsHandler(tornado.web.RequestHandler):
+    """ This will GET processed data metrics for events
+    stored in the DB, found by event_id.
+    """
+    def get(self, event_id):
+        try:
+            event = retrieve_from_db(event_id)
+            self.write(event.serialize())
+        except OSError as e:
+            self.respond(e.message, e.code)
+
+
+def respond(self, data, code=200):
+    self.set_status(code)
+    self.write(JSONEncoder().encode({
+        "status": code,
+        "data": data
+    }))
+    self.finish()
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/event/?", InteractionEventHandler)
+        (r"/event/(?P<actor>[^\/]+)/?(?P<entity>[^\/]+)?/", EventHandler),
+        (r"/event_metrics/(\d+)", EventMetricsHandler)
     ])
 
 if __name__ == '__main__':
     app = make_app()
-    app.listen(8080)
+    app.listen(8000)
     tornado.ioloop.IOLoop.current().start()
